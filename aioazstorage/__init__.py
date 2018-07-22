cee50db81500936b7df035b569d29ede5cc1a754
Rui Carmo <rui.carmo@gmail.com>
2018-07-22 11:46:47 +0100

    table insertion

--- aioazstorage/__init__.py
+++ aioazstorage/__init__.py
@@ -2,6 +2,7 @@ from aiohttp import ClientSession
 from asyncio import sleep
 from base64 import b64encode, b64decode
 from datetime import datetime
+from dateutil import parser
 from email.utils import formatdate
 from hashlib import sha256, md5
 from hmac import HMAC
@@ -12,6 +13,22 @@ try:
     from ujson import dumps
 except ImportError:
     from json import dumps
+
+
+_edm_types = {
+    datetime: "Edm.DateTime",
+    int: "Edm.Int64"
+}
+
+_edm_formatters = {
+    datetime: lambda d: d.utcnow().isoformat(),
+    int: lambda d: str(d)
+}
+
+_edm_parsers = {
+    "Edm.DateTime": lambda d: parse(d),
+    "Edm.Int64": lambda d: int(d)
+}
 
 
 class StorageClient:
@@ -95,3 +112,32 @@ class StorageClient:
         canon = "/{}/Tables('{}')".format(self.account, name)
         uri = "https://{}.table.core.windows.net/Tables('{}')".format(self.account, name)
         return await self.session.delete(uri, headers=self._sign_for_tables(canon))
+
+
+    def _annotate_payload(self, payload):
+        for k in list(payload.keys()):
+            t = type(payload[k])
+            if t in _edm_types:
+                payload[k+'@odata.type'] = _edm_types[t]
+            if t in _edm_formatters:
+                payload[k] = _edm_formatters[t](payload[k])
+        return payload
+
+    def _parse_payload(self, payload):
+        for k in list(payload.keys()):
+            edm = payload.get(k+'@odata.type', None)
+            if edm in _edm_parsers:
+                payload[k] = _edm_parsers[edm](payload[k])
+        # remove EDM annotations
+        for k in list(payload.keys()):
+            if '@odata.type' in k:
+                del payload[k]
+        return payload
+
+
+    async def insertEntity(self, table, entity={}):
+        """Create a new entity"""
+        canon = '/{}/{}'.format(self.account, table)
+        uri = 'https://{}.table.core.windows.net/{}'.format(self.account, table)
+        payload = dumps(self._annotate_payload(entity))
+        return await self.session.post(uri, headers=self._sign_for_tables(canon, payload), data=payload)
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from asyncio import get_event_loop
 from aioazstorage import StorageClient
 from os import environ
+from datetime import datetime
+from uuid import uuid1
 
 # TODO: add SAS token support, reference:
 # https://github.com/yokawasa/azure-functions-python-samples/blob/master/blob-sas-token-generator/function/run.py
@@ -14,6 +16,19 @@ async def main():
     #print(await t.createTable('aiotest'))
     async for item in t.getTables({"$filter": "TableName eq 'aiotest'"}):
         print(item)
+    for i in range(10):
+        print(await t.insertEntity('aiotest', {  
+            "Address":"Mountain View",
+            "Age":23 + i,  
+            "AmountDue":200.23,  
+            "CustomerCode": str(uuid1()),
+            "CustomerSince@odata.type": "Edm.DateTime",  
+            "CustomerSince":datetime.now(),
+            "IsActive": True,  
+            "NumberOfOrders": 255,
+            "PartitionKey":"mypartitionkey",  
+            "RowKey": "Customer%d" % i
+        }))
     await t.close()
 
 if __name__ == '__main__':
